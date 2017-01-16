152c2a5a2314dae7d5ecc2a21f5ef96b199c0656
JuncoJet <jijivip@126.com>
2017-01-16 17:15:12 +0800

    新增带负载均衡的存活检测

--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 如何使用
 -
 * Windows 下装 Python 环境后直接双击运行，程序开发使用标准库，无需安装其他第三方程序。用户可根据需求改写 skeepalive.py、killserver.bat、startserver.bat 的代码。
-* Linux、BSD、OS X ~~下安装 Python 环境~~默认已有Python 2.x，可以输入以下命令运行`python skeepalive.py`，或者`chmod +x skeepalive.py`、`./skeepalive.py`执行。用户可根据需求改写 skeepalive.py、killserver.bat、startserver.bat 的代码。（你需要把bat改写成sh代码:-P）
+* Linux、BSD、OS X ~~下安装 Python 环境~~默认已有Python 2.x，可以输入以下命令运行`python skeepalive.py`，或者`chmod +x skeepalive.py`、`./skeepalive.py`执行。用户可根据需求改写 skeepalive.py、killserver.bat、startserver.bat 的代码。（你需要把bat改写成sh代码:-P）。
+* `skeepalivex.py` 是在原有基础上新增开发了带负载均衡的存活检测程序，能够在服务器检测失败之后自动切换到其他镜像服务器，使用方法类似上面。
 
 更新日志
 -
+* 2017-1-16 17:08:57 加入负载均衡模块，当服务器挂掉时可以自动切换服务器地址（初步实现基本功能、尚不完善）
 * 2017-1-5 09:47:42 增加 Linux 支持，调整配置参数，之前的检测过于激进
 * 2017-1-5 08:57:25 更新 OS X 支持，修正一处BUG
---
+++ skeepalivex.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+import os,socket,threading
+import zlib,urllib,time
+from bottle import route,run,template
+
+def killserver():
+    #os.system('killserver.bat')#Windows
+    #os.system('./killserver.x.sh')#OS X
+    os.system('./killserver.sh')#Linux
+def startserver():
+    global vv
+    vv=0#重置页面基准
+    #os.system('startserver.bat')#Windows
+    #os.system('./startserver.x.sh')#OS X
+    os.system('./startserver.sh')#Linux
+def restartserver():
+    killserver()
+    time.sleep(3)
+    startserver()
+startserver()#随脚本一同启动服务
+ERRTIMES=5#允许出错次数
+serverlist=[{
+    "ip":"127.0.0.1",#http/https服务器地址
+    "port":"8000",#http/https服务器端口
+    "stat":0,#记录服务器状态
+    "index":0#优先级，数值越小越优先，数值相同为平行优先级。
+    #区别说明：当高优先级的服务器恢复后会自动切回高优先级的服务器，平行优先级的服务器顺序轮流使用。
+},{
+    "ip":"127.0.0.1",
+    "port":"8100",
+    "stat":0,
+    "index":1
+}]
+socket.setdefaulttimeout(10)#连接超时
+@route('/pac')
+def pac():#获取负载均衡的pac文件
+    return template("pac",serverip=ip,serverport=port)
+@route('/info')
+def info():#获取服务器状态
+    serverstat={
+        'ip':'',
+        'port':''
+    }
+    info=template(u'服务器{{serverip}}:{{serverport}} 状态{{serverstat}}',
+                  serverip=ip,
+                  serverport=port,
+                  serverstat=serverstat)
+    return info
+#启动http服务器，用于生产负载均衡的pac文件
+#threading.Thread(target=run, args=(host='localhost', port=8080)).start()
+threading.Thread(target=run, args=()).start()#上面语法有误，暂时用默认值
+i,v,vv=0,0,0
+ip,port="",""
+while 1:
+    for server in serverlist:
+        try:
+            ip,port=server["ip"],server["port"]
+            proxies={
+                'http':'http://%s:%s'%(ip,port),#手动设置http代理
+                'https':'https://%s:%s'%(ip,port)#手动设置https代理
+                #'http':os.getenv("http_proxy")#从系统获取http代理（Linux支持）
+            }
+            t=urllib.urlopen(
+                'http://www.google.com/intl/',
+                 proxies=proxies,
+            )
+            v=zlib.crc32(t.read())
+            if v!=vv and vv:
+                #页面数据不一致处理
+                i+=1
+                print "error","%s:%s"%(ip,port),v,vv,i
+                if i>ERRTIMES:
+                    #重启服务
+                    restartserver()
+                    i=0
+            else:
+                #页面数据一致处理
+                print "well","%s:%s"%(ip,port),i
+                i=0#清空计数器，这里不清空可以累计出错次数，清空为连续出错次数
+            vv=v
+        except Exception,e:
+            #超时或无法连接处理
+            i+=1
+            print "%s:%s"%(ip,port),e,i
+            if i>ERRTIMES:
+                #重启服务
+                restartserver()
+                i=0
+    time.sleep(10)#检测间隔时间
